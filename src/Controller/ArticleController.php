61de3e10e9f66b6fec9b7b7a0d9a9e63e102245f
wewes2013 <wewes2013@gmail.com>
2018-06-15 11:09:00 -0400

    making progress

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,8 +8,25 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response(" Hello everybody!");
+    }
 
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+
+        return new Response(sprintf('Nouveau article: %s',$slug));
+    }
 }
